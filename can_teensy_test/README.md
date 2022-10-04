a4ce3b2ee71b3ab467895a47e6c70ceff5f9c141
offi-DtrGuo-cial <derekguo31@gmail.com>
2022-10-03 19:40:55 -0500

    Build while linking to the CAN library

---
+++ can_teensy_test/README.md
@@ -0,0 +1,3 @@
+Simple local program to test the PlatformIO (PIO) DE as well as compiling for the Teensy using NFR's custom CAN library.
+
+PIO automates dependency management, this is managed via the platformio.ini file. This does not absolve dependency inclusion in programs to use their contents, with some dependencies requiring inclusion to build properly.
--- can_teensy_test/src/main.cpp
+++ can_teensy_test/src/main.cpp
@@ -1,16 +1,19 @@
 // Receive test (fake) data from an ESP32 in the CAN format and print to serial.
 // This tests the functionality of PIO compilation as well as using the CAN library.
 
-// If Arduino libraries aren't linked in native VS Code, this will complain about missing libraries
+// If Arduino libraries aren't linked in native VS Code, this may complain about missing libraries
 // However, as long as platform.ini has an Arduino framework configured, this can be ignored and the project will compile.
 #include <Arduino.h>
+
+// From custom CAN lirary, must include to build for Teensy properly
+#include "teensy_can.h"
 
 void setup() {
     // Initialize Serial
     while(!Serial);
     Serial.begin(9600);
     delay(100);
-    Serial.println!("Teensy CAN data test:")
+    Serial.println("Teensy CAN data test:");
 }
 
 void loop() {
