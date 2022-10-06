45231d9b28d91fba28d2508e252c4dcc3f50f5f9
offi-DtrGuo-cial <derekguo31@gmail.com>
2022-10-05 19:44:45 -0500

    Program now continuously prints to serial

--- can_teensy_test/README.md
+++ can_teensy_test/README.md
@@ -1,3 +1,10 @@
 Simple local program to test the PlatformIO (PIO) DE as well as compiling for the Teensy using NFR's custom CAN library.
 
 PIO automates dependency management, this is managed via the platformio.ini file. This does not absolve dependency inclusion in programs to use their contents, with some dependencies requiring inclusion to build properly.
+
+Running your program on the Teensy takes the following simple steps:
+1) Connect the microUSB on the Teensy to your computer. NFR's microUSB cables do not have Thunderbolt terminals, so an additional converter will be necessary if, say, you're working on a Mac.
+2) Open the PIO menu, then open teensy40/General. 
+3) Open the Teensy loader program by clicking Upload.
+4) On your connected Teensy, press the small white button on the opposite end of the microUSB. This will automatically flash your program—PIO and Teensy handle dirty work like searching for ports and rebooting for you.
+5) Check your program status/Serial output by clicking Monitor.
--- can_teensy_test/src/main.cpp
+++ can_teensy_test/src/main.cpp
@@ -1,6 +1,8 @@
 // Receive test (fake) data from an ESP32 in the CAN format and print to serial.
 // This tests the functionality of PIO compilation as well as using the CAN library.
 
+using namespace std;
+
 // If Arduino libraries aren't linked in native VS Code, this may complain about missing libraries
 // However, as long as platform.ini has an Arduino framework configured, this can be ignored and the project will compile.
 #include <Arduino.h>
@@ -8,7 +10,26 @@
 // From custom CAN lirary, must include to build for Teensy properly
 #include "teensy_can.h"
 
+// Initialize CAN bus, number 1
+TeensyCAN<1> can_bus{};
+
+// Signal instantiation to be used for messages
+// Each signal is to be used for 1 message, and is constructed via template class
+// These signals are labeled for reception (RX)
+CANSignal<float, 0, 16, CANTemplateConvertFloat(0.01), CANTemplateConvertFloat(0), true> float_rx_sig{};
+CANSignal<uint8_t, 16, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0), false> uint8_t_rx_sig{};
+CANSignal<bool, 24, 1, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0), false> bool_rx_sig{};
+CANSignal<uint32_t, 32, 32, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0), false> ms_rx_sig{};
+
+// Message objects
+// Number of signals is templated arg, constructor takes bus, a message ID, and the signal objects (variable length)
+CANRXMessage<4> rx_msg{can_bus, 0x200, float_rx_sig, uint8_t_rx_sig, bool_rx_sig, ms_rx_sig};
+
 void setup() {
+    // Initialize CAN bus with RX message (defined above) and standardized baud rate (from library)
+    can_bus.RegisterRXMessage(rx_msg);
+    can_bus.Initialize(ICAN::BaudRate::kBaud1M);
+
     // Initialize Serial
     while(!Serial);
     Serial.begin(9600);
@@ -17,5 +38,19 @@ void setup() {
 }
 
 void loop() {
+    // Get data from signals
+    float rx_float = float_rx_sig;
+    uint8_t rx_uint8_t = uint8_t_rx_sig;
+    bool rx_bool = bool_rx_sig;
+    int32_t rx_ms = ms_rx_sig;
 
+    // rx_msg automatically updates from interrupt, can be printed from immediately
+    Serial.print("Received float ");
+    Serial.print(rx_float);
+    Serial.print(", uint8_t ");
+    Serial.print(rx_uint8_t);
+    Serial.print(", bool ");
+    Serial.print(rx_bool);
+    Serial.print(", time ");
+    Serial.println(rx_ms);
 }
