8b5672040423122ac4b217a4118affa91622ed65
Isabel Zhong <isabelzhong30@gmail.com>
2023-06-08 16:03:56 -0500

    tick faster

--- bs_3/src/main.cpp
+++ bs_3/src/main.cpp
@@ -10,37 +10,41 @@
 VirtualTimerGroup timer_group;
 
 /********** PROGRAM **********/
-void setup() {
-  #ifdef TELEMETRY_BASE_STATION_TX
-    Serial.begin(9600);
-  #endif
-  #ifdef TELEMETRY_BASE_STATION_RX
-    Serial.begin(115200);
-  #endif
+void setup()
+{
+#ifdef TELEMETRY_BASE_STATION_TX
+  Serial.begin(9600);
+#endif
+#ifdef TELEMETRY_BASE_STATION_RX
+  Serial.begin(115200);
+#endif
 
   // Set up device and check success
-  if (!telemetry_setup()) {
+  if (!telemetry_setup())
+  {
     Serial.println("Device setup failed");
-    while(1) {}
+    while (1)
+    {
+    }
   }
 
-  // Add tasks to timer
-  #ifdef TELEMETRY_BASE_STATION_TX
-    // Serial.println("CAN-LoRa test: TX");
-    timer_group.AddTimer(T_CS, tx_tick_fast);
-    timer_group.AddTimer(T_SEC, tx_tick_slow);
-    timer_group.AddTimer(T_MS, tx_send);
+// Add tasks to timer
+#ifdef TELEMETRY_BASE_STATION_TX
+  // Serial.println("CAN-LoRa test: TX");
+  timer_group.AddTimer(T_MS, tx_tick_fast);
+  timer_group.AddTimer(T_SEC, tx_tick_slow);
+  timer_group.AddTimer(T_MS, tx_send);
+  // timer_group.AddTimer(1U, tx_task);
+#endif
 
-    // timer_group.AddTimer(1U, tx_task);
-  #endif
-
-  #ifdef TELEMETRY_BASE_STATION_RX
-    // Serial.println("CAN-LoRa test: RX"); 
-    timer_group.AddTimer(T_MS, rx_task);
-  #endif
+#ifdef TELEMETRY_BASE_STATION_RX
+  // Serial.println("CAN-LoRa test: RX");
+  timer_group.AddTimer(T_MS, rx_task);
+#endif
 }
 
-void loop() {
+void loop()
+{
   // Tick once, check/update all groups within
   timer_group.Tick(millis());
 }
